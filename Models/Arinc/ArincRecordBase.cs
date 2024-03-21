b33fb9685259705852d544065b609373c366f1ef
Patrick Trunkl <patrick.trunkl@skylinx.aero>
2024-03-21 18:32:07 +0000

    parser implementation work

--- Models/Arinc/ArincRecordBase.cs
+++ Models/Arinc/ArincRecordBase.cs
@@ -1,6 +1,11 @@
+using Pidgin;
+
 namespace navdata.models.arinc
 {
     public abstract class ArincRecordBase {
-        
+        //Record Type is either S for standard or T for tailored
+        protected static readonly Parser<char, char> RECORD_TYPE_PARSER = Pidgin.Parser.Char('S').Or(Pidgin.Parser.Char('T'));
+        //Blank spacing parser
+        protected static readonly Parser<char, Unit> BLANK_SPACING_PARSER = Pidgin.Parser.SkipWhitespaces;
     }
 }
--- Models/Arinc/Header/HeaderOneRecord.cs
+++ Models/Arinc/Header/HeaderOneRecord.cs
@@ -1,66 +1,102 @@
+using System.Security.Cryptography;
 using Microsoft.AspNetCore.StaticFiles.Infrastructure;
 using Pidgin;
 
 namespace navdata.models.arinc {
     public class HeaderOneRecord {
-        public int Number { get; set; }
-        public string FileName { get; set; }
-        public int VersionNumber { get; set; }
-        public bool IsProduction { get; set; }
-        public int RecordLength { get; set; }
-        public uint RecordCount { get; set; }
-        public string CycleDate { get; set; }
-        public string CreationDate { get; set; }
-        public string CreationTime { get; set; }
-        public string? DataSupplierIdent { get; set; }
-        public string? TargetCustomerIdent { get; set; }
-        public string DatabasePartNumber { get; set; }
-        public string FileCRC { get; set; }
-    
-    
-        public static readonly Parser<string, HeaderOneRecord> PARSER = Pidgin.Parser.Map(
-            record => new HeaderOneRecord {
-
-            },
-            HEADER_IDENT_PARSER,
-            HEADER_NUMBER_PARSER,
-            FILE_NAME_PARSER,
-            VERSION_NUMBER_PARSER,
-            PRODUCTION_FLAG_PARSER,
-            RECORD_LENGH_PARSER,
-            RECORD_COUNT_PARSER,
-            CYCLE_DATE_PARSER,
-            CREATION_DATE_PARSER,
-            CREATION_TIME_PARSER,
-            DATA_SUPPLIER_IDENT,
-            TARGET_CUSTOMER_IDENT,
-            DATABASE_PART_NUMBER,
-            FILE_CRC_PARSER            
-        ); 
 
         private static readonly Parser<char, string> HEADER_IDENT_PARSER = Pidgin.Parser.String("HDR");
-        private static readonly Parser<char, int> HEADER_NUMBER_PARSER = Pidgin.Parser.DecimalNum;
-        private static readonly Parser<char, string?> FILE_NAME_PARSER = Pidgin.Parser.Map(
-            x => x.ToString(),
-            Pidgin.Parser.LetterOrDigit.AtLeastOnceUntil(
-                Pidgin.Parser.Whitespace)
+
+        private static readonly Parser<char, int> RECORD_NUMBER_PARSER = Pidgin.Parser.Map(
+            (rawNumber) => int.Parse(rawNumber.ToArray()),
+                Pidgin.Parser.Char('0').Repeat(1)
+            .Or(
+                Pidgin.Parser.Digit.Repeat(2)
+            )
         );
-        private static readonly Parser<char, string> VERSION_NUMBER_PARSER;
+
+        private static readonly Parser<char, string> FILE_NAME_PARSER = Pidgin.Parser.Map(
+            (rawFileName) => rawFileName.ToString() ?? string.Empty,
+            Pidgin.Parser.AnyCharExcept(new char[] {}).Repeat(15)
+        );
+
+        private static readonly Parser<char, int> VERSION_NUMBER_PARSER = Pidgin.Parser.Map(
+            (rawDigits) => int.Parse(rawDigits.ToArray()),
+            Pidgin.Parser.Digit.Repeat(3)
+        );
+
         private static readonly Parser<char, bool> PRODUCTION_FLAG_PARSER = Pidgin.Parser.Map(
-            x => x == 'T' ? false : true,
+            (value) => char.ToUpper(value) == 'T' ? false : true,
             Pidgin.Parser.Char('T').Or(Pidgin.Parser.Char('P'))
         );
-        private static readonly Parser<char, int> RECORD_LENGH_PARSER = Pidgin.Parser.DecimalNum;
-        private static readonly Parser<char, int> RECORD_COUNT_PARSER = Pidgin.Parser.DecimalNum;
-        private static readonly Parser<char, string?> CYCLE_DATE_PARSER = Pidgin.Parser.Map(
-            value => value.ToString(),
-            Pidgin.Parser.AnyCharExcept(new char[] {}).Repeat(4)
+
+        private static readonly Parser<char, int> RECORD_LENGTH_PARSER = Pidgin.Parser.Map(
+            (rawNumber) => int.Parse(rawNumber.ToArray()),
+            Pidgin.Parser.Digit.Repeat(4)
         );
-        private static readonly Parser<char, DateOnly> CREATION_DATE_PARSER; //TODO: Create parser
-        private static readonly Parser<char, TimeOnly> CREATION_TIME_PARSER; //TODO: Create parser
-        private static readonly Parser<char, char> DATA_SUPPLIER_IDENT;
-        private static readonly Parser<char, char> TARGET_CUSTOMER_IDENT;
-        private static readonly Parser<char, char> DATABASE_PART_NUMBER;
-        private static readonly Parser<char, int> FILE_CRC_PARSER = Parser.HexNum;
+
+        private static readonly Parser<char, long> RECORD_COUNT_PARSER = Pidgin.Parser.Map(
+            (rawNumber) => long.Parse(rawNumber.ToArray()),
+            Pidgin.Parser.Digit.Repeat(7)
+        );
+
+        private static readonly Parser<char, int> CYCLE_DATE_PARSER = Pidgin.Parser.Map(
+            (rawNumber) => int.Parse(rawNumber.ToArray()),
+            Pidgin.Parser.Digit.Repeat(4)
+        );
+
+        private static readonly Parser<char, DateOnly> CREATION_DATE_PARSER = Pidgin.Parser.Map(
+            (rawDate) => DateOnly.Parse(rawDate.ToArray()),
+            Pidgin.Parser.LetterOrDigit.Or(Pidgin.Parser.Char('-')).Repeat(11)
+        );
+
+        private static readonly Parser<char, TimeOnly> CREATION_TIME_PARSER = Pidgin.Parser.Map(
+            (rawTime) => TimeOnly.Parse(rawTime.ToArray()),
+            Pidgin.Parser.Digit.Or(Pidgin.Parser.Char(':')).Repeat(8)
+        );
+
+        private static readonly Parser<char, string> DATA_SUPPLIER_IDENT = Pidgin.Parser.Map(
+            (rawString) => rawString.ToString() ?? string.Empty,
+            Pidgin.Parser.AnyCharExcept(new char[] {}).Repeat(16)
+        );
+
+        private static readonly Parser<char, string> TARGET_CUSTOMER_IDENT = Pidgin.Parser.Map(
+            (rawString) => rawString.ToString() ?? string.Empty,
+            Pidgin.Parser.AnyCharExcept(new char[] {}).Repeat(16)
+        );
+
+        private static readonly Parser<char, string> DATABASE_PART_NUMBER = Pidgin.Parser.Map(
+            (rawString) => rawString.ToString() ?? string.Empty,
+            Pidgin.Parser.AnyCharExcept(new char[] {}).Repeat(20)
+        );
+
+        private static readonly Parser<char, int> FILE_CRC_PARSER = Pidgin.Parser.Map(
+            (rawNumber) => int.Parse(rawNumber.ToArray(), System.Globalization.NumberStyles.HexNumber),
+            Pidgin.Parser.AnyCharExcept(new char[] {}).Until(Parser.EndOfLine)
+        ); 
+
+        public static HeaderOneRecord? Parse(char[] sourceData) {
+            HeaderOneRecord record = new HeaderOneRecord();
+
+
+
+            return null;
+        } 
+
+        public int Number { get; }
+        public string FileName { get; }
+        public int VersionNumber { get; }
+        public bool IsProduction { get; }
+        public int RecordLength { get; }
+        public uint RecordCount { get; }
+        public string CycleDate { get; }
+        public string CreationDate { get; }
+        public string CreationTime { get; }
+        public string? DataSupplierIdent { get; }
+        public string? TargetCustomerIdent { get; }
+        public string DatabasePartNumber { get; }
+        public string FileCRC { get; }
+    
+
     }
 }
